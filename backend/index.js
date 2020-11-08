f5ac6a15b48fd0d7332bd284bebfe6e527368295
ink <ink@inkdeMacBook-Pro.local>
2020-11-08 20:20:10 +0800

    update backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const mysql = require('mysql');
 // parse application/json
 app.use(cors())
 app.use(bodyParser.json());
+app.use(express.static('public'));
 //create database connection
 const conn = mysql.createConnection({
   host: '47.94.194.104',
@@ -20,6 +21,18 @@ conn.connect((err) =>{
   if(err) throw err;
   console.log('Mysql Connected...');
 });
+app.get('/', function getState(req,res,next){
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+    res.sendFile('index.html')
+})
+app.get('/api/user/:id',(req, res) => {
+      let sql = "SELECT * FROM products WHERE name=\""+req.params.id+"\"" ;
+    console.log(sql)
+      let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      });
+    });
  app.get('/api/product',(req, res) => {
       let sql = "SELECT * FROM products";
       let query = conn.query(sql, (err, results) => {
@@ -34,6 +47,19 @@ console.log(sql)
     if(err) throw err;
     res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
   });
+});
+app.post('/neworder',function(req,res){
+    var id = req.body.id;//即productid
+    var userto = req.body.userto;
+    var userfrom = req.body.userfrom;
+    var sql = 'insert into orders set id=? ,userto=?, userfrom=?,state=0 '
+    conn.query(sql, [id,userto,userfrom], function (err, result) {
+        if (err) {
+            console.log(err)
+            console.log('新增数据失败');
+        }
+        res.send('增加数据成功') //   响应内容 增加数据成功
+    });
 });
 app.post('/add',function(req,res){
     var id = req.body.id;
@@ -72,6 +98,6 @@ app.put('/change',function(req,res){
     });
 });
 //Server listening
-app.listen(3001,() =>{
+app.listen(process.env.PORT || 3001,() =>{
   console.log('Server started on port 3001...');
 });
