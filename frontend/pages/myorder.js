2d83c38990d91b6971963504e6f4fc05f44bc590
11aaabbb <61314324+11aaabbb@users.noreply.github.com>
2020-12-07 22:27:35 +0800

    Add files via upload
    fix

--- frontend/pages/myorder.js
+++ frontend/pages/myorder.js
@@ -2,39 +2,33 @@ import Layout from '../components/layout'
 import { getSession } from 'next-auth/client'
 import {orderInfo} from '../utils/orderInfo'
 import * as moment from 'moment';
+import { Table } from '@geist-ui/react'
 function formatDate(momentDate) {        
     return moment(momentDate).format("MM/DD/YYYY hh:mm:ss");
   }
-export default function Page ({orders}) {
+
+export default function Page ({orders, product}) {
   const items=[]
-  for (var i=0;i<orders.length;i++){
-    items.push(
-    <tbody key ={i} className="orders"> 
-    <tr>
-    <td><a href={`/order/${orders[i].id}`}>{orders[i].id}</a></td>
-    <td><a href={"/product/"+orders[i].productid}>{orders[i].productname}</a></td>
-    <td>{formatDate(orders[i].time)}</td>
-    <td>从{orders[i].fromuser}手中购买</td>
-    <td>{orderInfo(orders[i].status)}</td>
-    </tr>
-</tbody>
-    )
+  for(var i=0; i<orders.length;i++)
+  {
+    let data = {}
+    data["订单编号"] = <a href={"/product/"+ orders[i].productid}>{orders[i].id}</a>
+    data["物品名称"] = <a href={"/product/"+ orders[i].productid}>{orders[i].productname}</a>
+    data["订购日期"] = formatDate(orders[i].time)
+    data["供货商"] = orders[i].fromuser
+    data["状态"] = orderInfo(orders[i].status)
+    items.push(data)
   }
   return (
     <Layout>
         <h2>以下是您订购的商品</h2>
-<table>
-    <thead>
-        <tr>
-            <th>订单编号</th>
-            <th>物品名称</th>
-            <th>订购日期</th>
-            <th>供货商</th>
-            <th>状态</th>
-        </tr>
-    </thead>
-    {items}
-</table>
+    <Table data={items}>
+      <Table.Column prop="订单编号" label="订单编号"/>
+      <Table.Column prop="物品名称" label="物品名称" />
+      <Table.Column prop="订购日期" label="订购日期" />
+      <Table.Column prop="供货商" label="供货商" />
+      <Table.Column prop="状态" label="状态"/>
+    </Table>
     </Layout>
   )
 }
@@ -42,7 +36,6 @@ export async function getServerSideProps(context) {
     // Call an external API endpoint to get posts.
     // You can use any data fetching library
     const session = await getSession(context)
-    //const session = await getSession(context)
     const res = await fetch('https://api.hezh.fail/myorder/'+session.user.name)
     const json = await res.json()
     const orders=json.response
